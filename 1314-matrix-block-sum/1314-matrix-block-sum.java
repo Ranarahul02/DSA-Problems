f1fa04d0f4441e707ce43945d50e20e2edae6da4
Rahul Kumar Rana <ranarahulkumar90@gmail.com>
2026-07-08 13:24:53 +0530

    Time: 7 ms (68.59%), Space: 46.9 MB (22.78%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -3,20 +3,34 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
 
+        int prefix[][] = new int[m+1][n+1];
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                prefix[i][j] = mat[i-1][j-1]
+                        + prefix[i-1][j]
+                        +prefix[i][j-1]
+                        -prefix[i-1][j-1];
+            }
+        }
+
         int res[][] = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int r1 = Math.max(0, i-k);
+                int c1 = Math.max(0, j-k);
+                int r2 = Math.min(m-1, i+k);
+                int c2 = Math.min(n-1, j+k);
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                int sum = 0;
+                r1++;
+                c1++;
+                r2++;
+                c2++;
 
-                for(int r=i-k;r<=i+k;r++){
-                    for(int c=j-k; c<=j+k;c++){
-                        if(r>=0 && c>=0 && r<m && c<n){
-                            sum += mat[r][c];
-                        }
-                    }
-                }
-                res[i][j] = sum;
+                res[i][j] = prefix[r2][c2]
+                            - prefix[r1-1][c2]
+                            - prefix[r2][c1-1]
+                            + prefix[r1-1][c1-1];
             }
         }
         return res;
