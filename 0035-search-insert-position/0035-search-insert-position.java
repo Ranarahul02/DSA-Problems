f62eaaaa177cc1e7ff01fa0c236500acc25b5e02
Rahul Kumar Rana <ranarahulkumar90@gmail.com>
2026-07-14 17:22:50 +0530

    Time: 0 ms (100%), Space: 45 MB (12.3%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int si = 0;
+        int ei = nums.length-1;
+    
+        while(si<=ei){
+            int mid=(si+ei)/2;
+            if(nums[mid]<target){
+                si = mid+1;
+            } else {
+                ei = mid-1;
+            }
+        }
+        return si;
+    }
+}
